df2f83a064d791fc596a6cf61f99dcf18f6d2fd0
zhangmin <zhangmin@gmail.com>
2017-06-16 10:29:42 +0800

    将背景和状态栏融合一起

--- app/src/main/java/com/coolweather/android/ui/activity/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/ui/activity/WeatherActivity.java
@@ -1,6 +1,8 @@
 package com.coolweather.android.ui.activity;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -37,6 +39,11 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {//5.0及其以上版本适用
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_weather);
         bingPicImg = (ImageView) findViewById(R.id.bing_pic_img);
         scrollView = (ScrollView) findViewById(R.id.weather_layout);
